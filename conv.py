92b8d42b1ba74401817426dcc977c76ddcc95dad
kenneth nondo <kennethnondo@gmail.com>
2026-02-19 20:47:18 +0200

    maybe

---
+++ conv.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+class CurrencyConverter:
+    def __init__(self):
+        window = Tk()
+        window.title("CurrencyConverter")
+        window.configure(bg="green")
+        Label(window,font="Helvetica 12 bold", bg="yellow", text="Amout to convert").grid(row=1, column=1,sticky=W)
+        Label(window,font="Helvetica 12 bold", bg="yellow", text="conversion Rate").grid(row=2, column=1,sticky=W)
+        Label(window,font="Helvetica 12 bold", bg="yellow", text="converted Amount").grid(row=3, column=1,sticky=W)
+        self.amounttoConvertVar = StringVar()
+        Entry(window, textvariable= self.amounttoConvertVar, justify=RIGHT).grid(row=1,column=2)
+        self.conversionRateVar = StringVar()
+        Entry(window, textvariable= self.conversionRateVar, justify= RIGHT).grid(row=2,column=2)
+        self.convertdAmountVar = StringVar()
+        lblConvertedAmount = Label(window,font="Helvetica 12 bold", bg="yellow", textvariable=self.convertdAmountVar).grid(row=3,column=2,sticky = E)
+
+        #creating button
+
+        btConvertedAmount = Button(window, text="Convert",font="Helvetica 12 bold", bg="blue", fg="white", command=self.ConvertedAmount).grid(row=4,column=2,sticky=E)
+        btConvertedAmount = Button(window, text="Convert",font="Helvetica 12 bold", bg="red", fg="white", command=self.ConvertedAmount).grid(row=4,column=3,sticky=E)
+
+        window.mainloop()
